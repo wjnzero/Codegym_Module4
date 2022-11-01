73718caac8a90e6d4ff0b9faf59b5775f3146307
wjnzero <chuckieakenzzz@gmai.com>
2022-11-01 13:36:34 +0700

    up i18n

---
+++ i18n/InternationalizationDemo/src/main/java/com/hug/controller/MainController.java
@@ -0,0 +1,28 @@
+package com.hug.controller;
+
+import com.hug.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+    @GetMapping("/login")
+    public ModelAndView showLoginForm() {
+        ModelAndView modelAndView = new ModelAndView("/login");
+        modelAndView.addObject("user", new User());
+        return modelAndView;
+    }
+
+    @PostMapping("/doLogin")
+    public ModelAndView doLogin(@ModelAttribute User user) {
+        if(user.getUsername().equals("hug") && user.getPassword().equals("123")){
+            ModelAndView modelAndView = new ModelAndView("/success");
+            modelAndView.addObject("user",user);
+            return modelAndView;
+        }
+        return new ModelAndView("/error");
+    }
+}
